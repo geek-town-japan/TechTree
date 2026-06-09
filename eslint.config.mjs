82a72870c1e7e3ac0d6c01cebe7b258a062003e0
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-06-09 19:28:26 +0900

    👷 ci: ESLintのルールの追加
    - シングルクォーテーションを使用していないかを検知するようにルール追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
+import prettier from "eslint-config-prettier/flat"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  prettier,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  {
+    rules: {
+      quotes: ["error", "double", { avoidEscape: true }]
+    }
+  }
+])
 
-export default eslintConfig;
+export default eslintConfig
