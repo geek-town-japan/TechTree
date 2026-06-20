640d14cc9615e97585cdeba8e148022f5e98299c
dev-hayato-shiina <dev.hayato.shiina@gmail.com>
2026-06-21 02:36:45 +0900

    👷 ci: Storybookの導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config"
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
+import storybook from "eslint-plugin-storybook"
 import prettier from "eslint-config-prettier/flat"
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...storybook.configs["flat/recommended"],
   prettier,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
   {
